900815886e08684e6adf1f72ec5ae3701b0d25f0
Thouma <richard_allcca_llano@hotmail.com>
2024-07-28 13:52:47 -0500

    added slint

--- index.js
+++ index.js
@@ -1,5 +1,6 @@
 const express = require('express')
 const { PORT } = require('./config')
+const { sendMailGmail, sendMailOutlook } = require('./src/node-mailer/sendMail')
 
 const app = express()
 app.use(express.json()) // for parsing application/json
@@ -8,5 +9,7 @@ app.use(express.urlencoded({ extended: true })) // for parsing application/x-www
 app.get('/', (req, res) => res.send('Hello World!'))
 
 app.listen(PORT, () => {
+  // sendMailGmail()
+  // sendMailOutlook()
   console.log(`Example app listening on port ${PORT}!`)
 })
